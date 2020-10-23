186902da7516a617de275f3f141a355166b1de95
darik88 <mr.darik88@gmail.com>
2020-10-23 14:54:21 +0300

    Initial Commit

---
+++ PersonalQuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuizApp
+//
+//  Created by Айдар Рахматуллин on 23.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
